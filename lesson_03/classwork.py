a11080ed3f7676a16becfddfaf14839b168bea63
Nikita Gushchik <nikita_fabled@mail.ru>
2022-05-20 21:40:05 +0300

    classwork

---
+++ lesson_03/classwork.py
@@ -0,0 +1,54 @@
+x = 17 / 2 * 3 + 2
+print(x)
+x = 17 / 2 * (3 + 2)                                                    
+print(x)
+x = 2 + 17 / 2 * 3
+print(x)
+x = (2 + 17) / 2 * 3
+print(x)                                                    
+x = 19 % 4 + 15 / 2 * 3
+print(x)
+x = 19 % (4 + 15) / 2 * 3
+print(x)                                               
+x = (15 + 6) - 10 * 4
+print(x)
+x = ((15 + 6) - 10) * 4
+print(x)
+x = 17 / 2 % 2 * 3
+print(x)
+x = 17 / 2 % (2 * 3)
+print(x)
+
+Vlad = 19                                                              
+
+Anya = 20                                                              
+
+Dasha = 20                                                             
+
+print(Anya + Vlad + Dasha)
+
+print((Anya + Vlad + Dasha) / 3)
+
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]          
+
+len_l = len(my_list)                                                   
+
+my_set = set(my_list)                                                  
+
+len_s = len(my_set)                                                    
+
+print(len_l - len_s)
+
+my_list[2:5] 
+
+print(my_list)                                                          
+
+my_list[2:5][::-1]
+
+print(my_list)                                                    
+
+size = 4                                                               
+
+kvadrat = [size*4, size*4, 2**(1/2)*size]
+                                       
+print(kvadrat)                                                         
